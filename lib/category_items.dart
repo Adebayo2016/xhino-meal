0415e15a0fb0c6c5d9b038f3345590d01912c0b8
Adebayo2016 <adewaleadebayo630@gmail.com>
2022-06-16 15:30:06 +0100

    Added titles

--- lib/category_items.dart
+++ lib/category_items.dart
@@ -12,6 +12,10 @@ class CategoryItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: const EdgeInsets.all(15),
+      child: Text(title),
+
+    );
   }
 }
