9e8208b83138def910ff3a3e9df05ef88596957a
Adebayo2016 <adewaleadebayo630@gmail.com>
2022-06-16 16:50:40 +0100

    Added stuffs sha

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -7,16 +7,19 @@ class CategoryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        mainAxisSpacing: 20,
-        crossAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(title: Text('my Meal'),),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES
+            .map((catData) => CategoryItem(catData.title, catData.color))
+            .toList(),
       ),
-      children: DUMMY_CATEGORIES
-          .map((catData) => CategoryItem(catData.title, catData.color))
-          .toList(),
     );
   }
 }
