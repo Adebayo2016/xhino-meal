a3ba714da93c38385f132d32de3f3c99a9bd4307
Adebayo2016 <adewaleadebayo630@gmail.com>
2022-06-16 16:38:48 +0100

    Fixed bug

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -2,22 +2,21 @@ import 'package:flutter/material.dart';
 import './dummy_data.dart';
 import './category_items.dart';
 
-
 class CategoryScreen extends StatelessWidget {
   const CategoryScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GridView(
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent:200,
-      childAspectRatio: 3/2,
-      mainAxisSpacing: 20,
-      crossAxisSpacing:20,
-
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        mainAxisSpacing: 20,
+        crossAxisSpacing: 20,
       ),
-      children: DUMMY_CATEGORIES.map((catData) => CategoryItem(catData.title,catData.color)).toList(),
-
-
+      children: DUMMY_CATEGORIES
+          .map((catData) => CategoryItem(catData.title, catData.color))
+          .toList(),
     );
   }
 }
