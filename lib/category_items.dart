2a342272d07823e91f5fe32249fd20b11a46a2d2
Adebayo2016 <adewaleadebayo630@gmail.com>
2022-06-16 17:56:28 +0100

    Added new thing

--- lib/category_items.dart
+++ lib/category_items.dart
@@ -5,25 +5,33 @@ class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
 
+
+  void SelectedCategory (){
+
+}
   CategoryItem(this.title, this.color);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(title),
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-        colors: [
-          color.withOpacity(0.7),
-          color,
-        ],
-            begin: Alignment.topLeft,
-            end:Alignment.bottomRight,
-      ),
-        borderRadius: BorderRadius.circular(17),
-      ),
+    return InkWell(
+      onTap: SelectedCategory,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(title),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+          colors: [
+            color.withOpacity(0.7),
+            color,
+          ],
+              begin: Alignment.topLeft,
+              end:Alignment.bottomRight,
+        ),
+          borderRadius: BorderRadius.circular(15),
+        ),
 
+      ),
     );
   }
 }
