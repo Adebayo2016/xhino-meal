c8c8d257e3093e10c10946e14af7e833766fcfba
Adebayo2016 <adewaleadebayo630@gmail.com>
2022-06-16 14:41:14 +0100

    updated categories

---
+++ lib/models/category.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class Category {
+  late final String id;
+  late final String title;
+  late final Color color;
+
+  Category({required this.id, required this.title, this.color = Colors.orange});
+
+}
