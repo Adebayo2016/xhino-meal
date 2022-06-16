ae86e0386db2e8e81b83bee2274c539d81d9c8cc
Adebayo2016 <adewaleadebayo630@gmail.com>
2022-06-16 23:37:58 +0100

    added sth

--- lib/category_items.dart
+++ lib/category_items.dart
@@ -4,17 +4,18 @@ import 'package:flutter/material.dart';
 import 'category_meal_screen.dart';
 
 class CategoryItem extends StatelessWidget {
+  final String id;
   final String title;
-
   final Color color;
 
+  CategoryItem(this.id,this.title, this.color);
 
   void SelectedCategory (BuildContext ctx){
     Navigator.of(ctx).push(MaterialPageRoute(builder: (_){
       return const CategoryMealsScreen();
     }));
 }
-  CategoryItem(this.title, this.color);
+
 
   @override
   Widget build(BuildContext context) {
