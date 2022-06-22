6003a4985d837e4fb8f9d3910f71c17c474881f1
Adebayo2016 <adewaleadebayo630@gmail.com>
2022-06-22 21:46:17 +0100

    Added some changes

--- lib/category_meal_screen.dart
+++ lib/category_meal_screen.dart
@@ -14,6 +14,8 @@ class CategoryMealsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final routeArgs=ModalRoute.of(context)!.settings.arguments as Map<String, String>;
     final title=routeArgs['id'];
+    final categoryId=routeArgs['title'];
+
     return Scaffold(
       appBar: AppBar(
         title:   Text(title!),
