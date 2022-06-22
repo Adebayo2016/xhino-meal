749bc13c6f3c4bcd9c9d01b50d700d8c4c526736
Adebayo2016 <adewaleadebayo630@gmail.com>
2022-06-22 21:59:57 +0100

    Fix some Bugs

--- lib/category_meal_screen.dart
+++ lib/category_meal_screen.dart
@@ -2,7 +2,6 @@
 import 'package:flutter/material.dart';
 class CategoryMealsScreen extends StatelessWidget {
 
-
  // const CategoryMealsScreen({Key? key, required this.CategoryId, required this.title}) : super(key: key);
  //  final String CategoryId;
  //  final String title;
