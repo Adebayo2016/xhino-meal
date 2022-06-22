2a9e6d8381d443ff58ff2c95bb26bb74ce077eb3
Adebayo2016 <adewaleadebayo630@gmail.com>
2022-06-22 21:33:47 +0100

    Added some changes

--- lib/category_items.dart
+++ lib/category_items.dart
@@ -8,35 +8,32 @@ class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
 
-  CategoryItem(this.id,this.title, this.color);
-  void SelectedCategory (BuildContext ctx){
-    Navigator.of(ctx).pushNamed('/category-meal',arguments: {
-      'id':id,
-      'title':title});
-    }
+  CategoryItem(this.id, this.title, this.color);
+
+  void SelectedCategory(BuildContext ctx) {
+    Navigator.of(ctx)
+        .pushNamed('/category-meal', arguments: {'id': id, 'title': title});
+  }
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap:() => SelectedCategory,
-
+      onTap: () => SelectedCategory,
       borderRadius: BorderRadius.circular(15),
       child: Container(
         padding: const EdgeInsets.all(15),
         child: Text(title),
         decoration: BoxDecoration(
-            gradient: LinearGradient(
-          colors: [
-
-            color.withOpacity(0.7),
-            color,
-          ],
-              begin: Alignment.topLeft,
-              end:Alignment.bottomRight,
-        ),
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
           borderRadius: BorderRadius.circular(15),
         ),
-
       ),
     );
   }
--- lib/category_meal_screen.dart
+++ lib/category_meal_screen.dart
@@ -1,11 +1,14 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 
 
 class CategoryMealsScreen extends StatelessWidget {
- const CategoryMealsScreen({Key? key, required this.CategoryId, required this.title}) : super(key: key);
-  final String CategoryId;
-  final String title;
 
+  final routeArgs=ModalRoute.of(context).settings.arguments as Map<String, String>;
+ // const CategoryMealsScreen({Key? key, required this.CategoryId, required this.title}) : super(key: key);
+ //  final String CategoryId;
+ //  final String title;
 
   @override
   Widget build(BuildContext context) {
